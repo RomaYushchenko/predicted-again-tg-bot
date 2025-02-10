ca91f01fc82bd37dd796c46c5c778fd02edb1a83
RomaYushchenko <yushenkoromaf7@gmail.com>
2025-02-10 20:15:56 +0200

    Fixed notification time zone for Kyiv

--- src/main/java/com/ua/yushchenko/command/TimeMessageCommand.java
+++ src/main/java/com/ua/yushchenko/command/TimeMessageCommand.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,7 +41,8 @@ public class TimeMessageCommand extends BaseMessageCommand {
 
         String timeStr = message.getText().trim();
         try {
-            LocalTime time = LocalTime.parse(timeStr, TIME_FORMATTER);
+            LocalTime time = LocalTime.parse(timeStr, TIME_FORMATTER).minusHours(2);
+
             LocalDateTime notificationTime = LocalDateTime.now()
                 .withHour(time.getHour())
                 .withMinute(time.getMinute())

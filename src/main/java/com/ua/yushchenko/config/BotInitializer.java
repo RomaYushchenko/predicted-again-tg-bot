4eb4175f11e7a22258f12d4563f98d9bc508b540
RomaYushchenko <33280684+RomaYushchenko@users.noreply.github.com>
2025-06-04 17:57:35 +0300

    Switch bot to webhook and auto register

--- src/main/java/com/ua/yushchenko/config/BotInitializer.java
+++ src/main/java/com/ua/yushchenko/config/BotInitializer.java
@@ -2,11 +2,13 @@
 package com.ua.yushchenko.config;
 
 import com.ua.yushchenko.bot.TelegramBot;
+import com.ua.yushchenko.config.BotConfig.BotSettings;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
@@ -22,14 +24,16 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 public class BotInitializer {
     /** The bot instance to be registered */
     private final TelegramBot bot;
+    private final BotSettings botSettings;
 
     /**
      * Creates a new BotInitializer instance.
      *
      * @param bot the bot instance to initialize
      */
-    public BotInitializer(TelegramBot bot) {
+    public BotInitializer(TelegramBot bot, BotSettings botSettings) {
         this.bot = bot;
+        this.botSettings = botSettings;
     }
 
     /**
@@ -42,9 +46,13 @@ public class BotInitializer {
     public void init() throws TelegramApiException {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         try {
-            telegramBotsApi.registerBot(bot);
+            SetWebhook setWebhook = SetWebhook.builder()
+                                             .url(botSettings.getWebhookUrl())
+                                             .build();
+            telegramBotsApi.registerBot(bot, setWebhook);
+            log.info("Webhook registered at {}", botSettings.getWebhookUrl());
         } catch (TelegramApiException e) {
             log.error("Error occurred: " + e.getMessage());
         }
     }
-} 
+}
